e6b8f2e11ddf92d1a3fd2ed611b97c1df8b78532
Andreas Watterott <aw@watterott.net>
2017-10-22 15:09:40 +0200

    added post-install-script

---
+++ watterott/avr/post_install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Linux Modem Manager
+if [ ! -f "/etc/udev/rules.d/77-mm-usb-device-blacklist.rules" ] && [ ! -f "/lib/udev/rules.d/77-mm-usb-device-blacklist.rules" ]; then
+  echo "Update Modem Manager rules..."
+  cat > /etc/udev/rules.d/77-mm-usb-device-blacklist.rules <<EOF
+ATTRS{idVendor}=="6666", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="1D50", ATTRS{idProduct}=="60B0", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="1D50", ATTRS{idProduct}=="6112", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="1D50", ATTRS{idProduct}=="6113", ENV{ID_MM_DEVICE_IGNORE}="1"
+EOF
+else
+  if ! grep -q 'ATTRS{idVendor}=="6666", ENV{ID_MM_DEVICE_IGNORE}="1"' '/etc/udev/rules.d/77-mm-usb-device-blacklist.rules'; then
+    echo "Update Modem Manager rules..."
+    cat >> /etc/udev/rules.d/77-mm-usb-device-blacklist.rules <<EOF
+ATTRS{idVendor}=="6666", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="1D50", ATTRS{idProduct}=="60B0", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="1D50", ATTRS{idProduct}=="6112", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="1D50", ATTRS{idProduct}=="6113", ENV{ID_MM_DEVICE_IGNORE}="1"
+EOF
+  fi
+fi
+
+# Micronucleus
+if [ ! -f "/etc/udev/rules.d/49-micronucleus.rules" ] && [ ! -f "/lib/udev/rules.d/49-micronucleus.rules" ]; then
+  echo "Update Micronucleus rules..."
+  cat > /etc/udev/rules.d/49-micronucleus.rules <<EOF
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="16d0", ATTRS{idProduct}=="0753", MODE:="0666", ENV{MTP_NO_PROBE}="1"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="16d0", ATTRS{idProduct}=="0753", MODE:="0666", ENV{ID_MM_DEVICE_IGNORE}="1"
+EOF
+fi
+
+# reload udev rules
+echo "Reload rules..."
+udevadm control --reload-rules
