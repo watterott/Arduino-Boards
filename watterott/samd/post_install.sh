ed1fef4649cadca1da4581f3dd5dee64ded8a890
Andreas Watterott <aw@watterott.net>
2017-12-02 15:39:40 +0100

    updated install scripts

--- watterott/samd/post_install.sh
+++ watterott/samd/post_install.sh
@@ -25,15 +25,6 @@ EOF
   fi
 fi
 
-# Micronucleus
-if [ ! -f "/etc/udev/rules.d/49-micronucleus.rules" ] && [ ! -f "/lib/udev/rules.d/49-micronucleus.rules" ]; then
-  echo "Update Micronucleus rules..."
-  cat > /etc/udev/rules.d/49-micronucleus.rules <<EOF
-SUBSYSTEMS=="usb", ATTRS{idVendor}=="16d0", ATTRS{idProduct}=="0753", MODE:="0666", ENV{MTP_NO_PROBE}="1"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="16d0", ATTRS{idProduct}=="0753", MODE:="0666", ENV{ID_MM_DEVICE_IGNORE}="1"
-EOF
-fi
-
 # reload udev rules
 echo "Reload rules..."
 udevadm control --reload-rules
